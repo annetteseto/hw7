57ab95995ac917e21d36ad3e6828832bcadb6ec5
annetteseto <annetteseto@gmail.com>
2017-02-25 20:05:40 -0800

    Add files via upload

---
+++ hw7.js
@@ -0,0 +1,44 @@
+var locations = [
+    [
+        "Business and Economics Building",
+        48.465269, 
+        -123.313234
+    ],
+    [
+        "Clearihue Building",
+        48.464280, 
+        -123.310455
+    ],
+    [
+        "Felicita's Campus Pub",
+        48.464800, 
+       -123.307923
+    ]
+]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(48.463649,  -123.311951),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+   var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+
+
